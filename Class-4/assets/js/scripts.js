736c3031719c80c6c7cabf02ee20324e1af6a847
Rayhanuc <kj\hf_yu9i[wehr>
2017-09-16 23:45:42 +0600

    Class-4
    psd to html 2 Class-4

---
+++ Class-4/assets/js/scripts.js
@@ -0,0 +1,38 @@
+jQuery(document).ready(function($) {
+	$(".homepage-slides").owlCarousel({
+		items: 1,
+		center: true,
+		loop: true,
+		nav: true,
+		dots: true,
+		navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+		autoplay: true
+	});
+
+	$("#web-design-skillbar").circleProgress({
+		value: 0.9,
+		size: 270,
+		thickness: 3,
+		fill: '#22BBC0',
+	}).on('circle-animation-progress', function(event, progress) {
+	    $(this).find('.skill-count-no').html(Math.round(90 * progress) + '<i>%</i>');
+	});
+
+	$("#graphic-design-skillbar").circleProgress({
+		value: 0.95,
+		size: 270,
+		thickness: 3,
+		fill: '#22BBC0',
+	}).on('circle-animation-progress', function(event, progress) {
+	    $(this).find('.skill-count-no').html(Math.round(95 * progress) + '<i>%</i>');
+	});
+
+	$("#digital-marketing-skillbar").circleProgress({
+		value: 0.84,
+		size: 270,
+		thickness: 3,
+		fill: '#22BBC0',
+	}).on('circle-animation-progress', function(event, progress) {
+	    $(this).find('.skill-count-no').html(Math.round(84 * progress) + '<i>%</i>');
+	});
+});
