d355d9cc76e3c19d72499146bf1ab2216168482e
Rayhanuc <kj\hf_yu9i[wehr>
2017-09-30 14:16:57 +0600

    map color
    Google color change

--- google_map_3/assets/js/scripts.js
+++ google_map_3/assets/js/scripts.js
@@ -1,20 +1,52 @@
-(function ($) {
-	"use stict";
+(function($) {
+	"use strict"
 
-	jQuery(document).ready(function($){
-		
-		var uluru = {lat: -25.363, lng: 131.044};
-		    $('.map')
-		      .gmap3({
-		        zoom: 4,
-		        center: uluru
-		      })
-		      .infowindow({
-		        position: uluru,
-		        content: "Hello from Uluru"
-		      })
-		      .then(function (infowindow) {
-		        infowindow.open(this.get(0)); // this.get(0) return the map (see "get" feature)
-		      });
-	})
-});
+	jQuery(document).ready(function($) {
+	 $('.map')
+      .gmap3({
+        center:[23.7964381,90.422746],
+        zoom:12,
+        mapTypeId: "shadeOfGrey", // to select it directly
+        mapTypeControlOptions: {
+          mapTypeIds: [google.maps.MapTypeId.ROADMAP, "shadeOfGrey"]
+        }
+      })
+      .styledmaptype(
+        "shadeOfGrey",
+        [
+          {"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#F5F6F7"},{"lightness":40}]},
+          {"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},
+          {"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
+          {"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},
+          {"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},
+          {"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#5A5A5A"},{"lightness":20}]},
+          {"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},
+          {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},
+          {"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},
+          {"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#C7CBD1"},{"lightness":18}]},
+          {"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},
+          {"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},
+          {"featureType":"water","elementType":"geometry","stylers":[{"color":"#9FD8FC"},{"lightness":17}]}
+        ],
+        {name: "Shades of Grey"}
+      );
+	});
+
+	jQuery(window).load(function() { 
+
+	});
+
+
+}(jQuery));
+
+
+
+
+
+					function myMap() {
+						var mapProp= {
+						    center:new google.maps.LatLng(23.4521847,85.8461677),
+						    zoom:5,
+						};
+						var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+						}
