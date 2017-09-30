53e08a107397825240b8696a6865df921c83dd68
Rayhanuc <kj\hf_yu9i[wehr>
2017-09-30 23:11:12 +0600

    Google_map_3
    some changes

--- google_map_3/assets/js/scripts.js
+++ google_map_3/assets/js/scripts.js
@@ -2,34 +2,26 @@
 	"use strict"
 
 	jQuery(document).ready(function($) {
-	 $('.map')
+	  var center = [22.3455632, 91.8140194];
+    $('.map')
       .gmap3({
-        center:[23.7964381,90.422746],
-        zoom:12,
-        mapTypeId: "shadeOfGrey", // to select it directly
-        mapTypeControlOptions: {
-          mapTypeIds: [google.maps.MapTypeId.ROADMAP, "shadeOfGrey"]
-        }
+        center: center,
+        zoom: 13,
+        mapTypeId : google.maps.MapTypeId.ROADMAP
       })
-      .styledmaptype(
-        "shadeOfGrey",
-        [
-          {"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#F5F6F7"},{"lightness":40}]},
-          {"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},
-          {"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
-          {"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},
-          {"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},
-          {"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#5A5A5A"},{"lightness":20}]},
-          {"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},
-          {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},
-          {"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},
-          {"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#C7CBD1"},{"lightness":18}]},
-          {"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},
-          {"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},
-          {"featureType":"water","elementType":"geometry","stylers":[{"color":"#9FD8FC"},{"lightness":17}]}
-        ],
-        {name: "Shades of Grey"}
-      );
+        .infowindow({
+        content: "Hello from Uluru"
+      })
+      .circle({
+        center: center,
+        radius : 750,
+        fillColor : "#FFAF9F",
+        strokeColor : "#FF512F"
+      })
+        .marker({
+        position: uluru
+      })
+      
 	});
 
 	jQuery(window).load(function() { 
